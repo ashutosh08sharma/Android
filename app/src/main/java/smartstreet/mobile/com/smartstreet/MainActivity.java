bc2b121dcf13b0e3c77d236d33589cf5382bc881
ashutosh08sharma <ashutosh08sharma@gmail.com>
2016-03-04 12:43:57 -0800

    first commit

---
+++ app/src/main/java/smartstreet/mobile/com/smartstreet/MainActivity.java
@@ -0,0 +1,99 @@
+package smartstreet.mobile.com.smartstreet;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+// Main page of app,, first activity to load when application is opened, contains six imagebutton and barcode in the bottom which redirects to Smart Street website
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+    /*
+        methods to redirect to different activity  by passing intent object in StartActivity for different activity
+     */
+    private void showMap()
+    {
+        Intent i = new Intent(this,nearbyPlaces.class);
+        startActivity(i);
+    }
+
+
+    private void scanTree()
+    {
+        Intent i = new Intent(this, barcodeScanner.class);
+        startActivity(i);
+    }
+
+    private void viewPhoto()
+    {
+        Intent i = new Intent(this, PhotoActivity.class);
+        startActivity(i);
+    }
+
+    private void showComment()
+    {
+        Intent i = new Intent(this, commentActivity.class);
+        startActivity(i);
+    }
+
+
+    private void viewShare()
+    {
+        Intent i = new Intent(this, shareActivity.class);
+        startActivity(i);
+    }
+
+    private void viewInteract() {
+        Intent i = new Intent(this, interactActivity.class);
+
+        startActivity(i);
+    }
+
+
+
+
+
+/*
+onClick method based on  button id it will redirect it to that particular activity on click event
+
+ */
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.mapButton:
+                showMap();
+                break;
+            case R.id.aboutButton:
+                scanTree();
+                break;
+            case R.id.photoButton:
+                viewPhoto();
+                break;
+            case R.id.commentButton:
+                showComment();
+                break;
+            case R.id.shareButton:
+                viewShare();
+                break;
+            case R.id.interactButton:
+                viewInteract();
+                break;
+
+        }
+    }
+
+}
---
+++ app/src/main/java/smartstreet/mobile/com/smartstreet/nearbyPlaces.java
@@ -0,0 +1,83 @@
+package smartstreet.mobile.com.smartstreet;
+
+import android.os.Bundle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlacePicker;
+
+
+/* Implemented nearbyPlaces class using google place api and place picker to implement location based search
+  */
+
+public class nearbyPlaces extends AppCompatActivity
+{
+        private static final int PLACE_PICKER_REQUEST = 1;
+        private TextView name;
+        private TextView address;
+        // when activity is started it run onCreate method
+        @Override
+        protected void onCreate(Bundle savedInstanceState)
+        {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_nearby_places); // set view of map
+            name = (TextView) findViewById(R.id.nameView); //
+            address = (TextView) findViewById(R.id.addressView);
+            Button pickerButton = (Button) findViewById(R.id.searchButton);
+            //add a listener to the button so that we can handle it
+            pickerButton.setOnClickListener(new View.OnClickListener() {
+
+/*
+To use Place Picker,create an intent and listen for the Activity result to retrieve the Place selected by the user.
+ */
+
+                @Override
+                public void onClick(View v) {
+                   // check for possible exception
+                    try
+                    {
+                        // created a intent for place picker
+                        PlacePicker.IntentBuilder intentBuilder = new PlacePicker.IntentBuilder();
+
+                        Intent intent = intentBuilder.build(nearbyPlaces.this);
+                        startActivityForResult(intent, PLACE_PICKER_REQUEST);// Start the intent by requesting a result,
+
+                    }
+                    catch (GooglePlayServicesRepairableException | GooglePlayServicesNotAvailableException e)
+                    {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        }
+
+    // A place has been received, used requestCode to track the request
+        @Override
+        protected void onActivityResult(int requestCode, int resultCode, Intent data)
+        {
+            if (requestCode == PLACE_PICKER_REQUEST && resultCode == Activity.RESULT_OK)
+            {
+                final Place place = PlacePicker.getPlace(this, data);
+                final CharSequence mName = place.getName();
+                final CharSequence mAddress = place.getAddress();
+
+
+                name.setText(mName);
+                address.setText(mAddress);
+            }
+                else
+                {
+                super.onActivityResult(requestCode, resultCode, data);
+                }
+        }
+}
